8361e8c8c542d17df658a155073bc2b1d09acea6
rudyt123 <chbuzz60@gmail.com>
2023-01-08 11:43:14 +0530

    better solution found

--- 3) Find second-largest number in list.py
+++ 3) Find second-largest number in list.py
@@ -1,19 +1,16 @@
 
-# this loop is used to find the largest value and set it as largest
-for no in list_1:
-    if no > Largest:
-        Largest = no
-Lrgst_determine.append(Largest)
-Largest = Lrgst_determine[0]
-
-# this loop is used to remove the largest int from new list
-for no in list_1:
-    l_2nd_help.append(no)
-    if Largest in l_2nd_help:
-        l_2nd_help.remove(Largest)
-
-for no_ in l_2nd_help:
-    if no_ > second_largest:
-        second_largest = no_
-
-print(second_largest)
+# to find second largest int in a list
+list_1 = [10,7,4,1,0,1]
+emt_list = []
+largest = list_1[0]
+for x in list_1:
+    if x > largest:
+        largest = x 
+    else:
+        pass
+list_1.remove(largest)
+largest = list_1[0]
+for x in list_1:
+    if x > largest:
+        largest = x 
+print(largest)
